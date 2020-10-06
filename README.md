5a54db1ec18af553115f07ee8a8fd3e7d229ec61
anagornaia <41295312+anagornaia@users.noreply.github.com>
2020-10-06 18:19:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# internship-java-practice
+
+
+Fork the project to your repository. Build project skipping tests: `mvn clean install -DskipTests`
+
+
+Run in your console `mvn test` thus you will run tests, and you will see some errors:
+
+````
+[ERROR] Errors: 
+[ERROR]   TasksTest.factorialTest:21 » UnsupportedOperation Not implemented yet!
+[ERROR]   TasksTest.fizzBuzzTest:51 » UnsupportedOperation Not implemented yet!
+[ERROR]   TasksTest.getDuplicateCharactersTest:72 » UnsupportedOperation Not implemented...
+[ERROR]   TasksTest.isPalindromeTest:65 » UnsupportedOperation Not implemented yet!
+[ERROR]   TasksTest.isPrimeTest:59 » UnsupportedOperation Not implemented yet!
+[ERROR]   TasksTest.isSubstringTest:80 » UnsupportedOperation Not implemented yet!
+[ERROR]   TasksTest.maximumTest:44 » UnsupportedOperation Not implemented yet!
+[ERROR]   TasksTest.reverseTest:38 » UnsupportedOperation Not implemented yet!
+[ERROR]   TasksTest.searchAlgorithmTest:88 » UnsupportedOperation Not implemented yet!
+[ERROR]   TasksTest.searchTest:29 » UnsupportedOperation Not implemented yet!
+[ERROR]   TasksTest.sortTest:97 » UnsupportedOperation Not implemented yet!
+[ERROR]   TasksTest.sumTest:12 » UnsupportedOperation Not implemented yet!
+[INFO]
+[ERROR] Tests run: 12, Failures: 0, Errors: 12, Skipped: 0
+[INFO]
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD FAILURE
+[INFO] ------------------------------------------------------------------------
+
+  ````
+  
+  Your task is to make all tests pass.
---
+++ src/main/java/com/endava/qa/internship/Tasks.java
@@ -0,0 +1,112 @@
+package com.endava.qa.internship;
+
+public class Tasks {
+
+
+    /**
+     * Sum two numbers
+     */
+    public static Integer sum(Integer a, Integer b) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * Write a method that calculates the factorial of a given number.
+     * Factorial is the product of all positive integers less than or equal to n. For example, factorial(4) = 4x3x2x1 = 24.
+     * TIP: To make it more interesting, try to do it recursively.
+     */
+    public static Double factorial(Integer a) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * Write a method that returns the index of the first occurrence of given integer in a list.
+     * Assume that the index of the first element in the list is zero.
+     * If the number does not exist return -1.
+     */
+    public static Integer search(Integer n, Integer[] list) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * Write a method that reverses a string.
+     * For example, 'java interview' becomes 'weivretni avaj'.
+     */
+    public static String reverse(String s) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * Write a method that returns the largest integer in the list.
+     * You can assume that the list has at least one element.
+     */
+    public static Integer maximum(Integer[] list) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * Write a method that returns 'Fizz' for multiples of three and 'Buzz' for the multiples of five.
+     * For numbers which are multiples of both three and five return 'FizzBuzz'.
+     * For numbers that are neither, return the input number.
+     */
+    public static String fizzBuzz(Integer i) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+     * Write a method that checks if a number is a prime number.
+     */
+    public static Boolean isPrime(Integer n) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * A palindrome is a word which reads the same backward or forward. 'abcba' is a palindrome.
+     * Write a method that detects if a string is a palindrome.
+     * Tip: Use word.charAt(i) to get the character at position i.
+     */
+    public static Boolean isPalindrome(String word) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+
+    /**
+     * Return duplicate characters from String.
+     * For example, if String is "Java" then the program should print "a". Bonus points if your program is robust and
+     * handles different kinds of input e.g. String without duplicate, null or empty String etc.
+     */
+
+    public static char[] getDuplicateCharacters(String string) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * Check if a String contains another String
+     * You need to write a function to search for the existence of a string (target) in another string (source).
+     * The function takes two strings as the input and returns true if substring was found and false if the target string cannot be found.
+     * Tip: U can use word.charAt(i) and/or word.toCharArray() to get the character at position i.
+     */
+
+    public static boolean isSubstring(String source, String target) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * Implement Binary Search Algorithm or if you like Linear search Algorithm.
+     * Method will return index of searched element.
+     * If element does not appear in input return -1
+     */
+    public static int search(int[] input, int number) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * Implement any sorting algorithm u like: Insertion sort, Selection sort, Bubble sort, Heap sort, Merge sort, Quick sort
+     */
+    public static int[] sort(int... d) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+
+}
